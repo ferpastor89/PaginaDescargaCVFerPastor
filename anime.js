42ca406f7c45afb4a4456b1be81b552e803a2a13
Fernando Pastor <fernandoandres.pastor@gmail.com>
2023-05-29 19:34:26 -0300

    Cambios varios en layout

--- anime.js
+++ anime.js
@@ -1,30 +1,32 @@
-
 document.addEventListener('DOMContentLoaded', function() {
   const elements = document.querySelectorAll('.staggering-grid-demo .el');
 
   anime({
     targets: elements,
     scale: [
-      { value: .1, easing: 'easeOutSine', duration: 500 },
+      { value: 0.1, easing: 'easeOutSine', duration: 500 },
       { value: 1, easing: 'easeInOutQuad', duration: 1200 }
     ],
-    delay: anime.stagger(200, { grid: [14, 5], from: 'center' }),
+    delay: anime.stagger(200, { grid: [14, 5], from: 'center' }).start,
     complete: function() {
       // Ocultar los elementos
       elements.forEach(element => {
         element.parentNode.removeChild(element);
       });
+
+      // Mostrar los iconos de las redes sociales en el centro de staggering-grid-demo
+      const iconContainer = document.querySelector('.icon-container');
+      const staggeringGridDemo = document.querySelector('.staggering-grid-demo');
+      const staggeringGridDemoRect = staggeringGridDemo.getBoundingClientRect();
+      const iconContainerRect = iconContainer.getBoundingClientRect();
+
+      const offsetX = staggeringGridDemoRect.left + (staggeringGridDemoRect.width - iconContainerRect.width) / 2;
+      const offsetY = staggeringGridDemoRect.top + (staggeringGridDemoRect.height - iconContainerRect.height) / 2;
+
+      iconContainer.style.display = 'flex';
+      iconContainer.style.position = 'absolute';
+      iconContainer.style.left = offsetX + 'px';
+      iconContainer.style.top = offsetY + 'px';
     }
   });
 });
-
-d
-
-
-  
-
-
-
-
-
-
--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-document.getElementById('downloadButton').addEventListener('click', function() {
-    // Aquí puedes colocar la lógica para generar y descargar el archivo PDF.
-    // Por simplicidad, este ejemplo simplemente abre una nueva ventana con un mensaje.
-    window.open('https://ruta-al-archivo.pdf');
-  });
+// Obtener el botón de descarga
+const downloadBtn = document.getElementById('downloadBtn');
 
-  ocument.addEventListener('DOMContentLoaded', function() {
-    const elements = document.querySelectorAll('.staggering-grid-demo .el');
-  
-    const iconsContainer = document.createElement('div');
-    iconsContainer.classList.add('icons-container');
-  
-    const githubLink = document.createElement('a');
-    githubLink.href = 'https://github.com/ferpastor89';
-    githubLink.target = '_blank';
-    const githubIcon = document.createElement('i');
-    githubIcon.classList.add('fa-brands fa-github');
-    githubLink.appendChild(githubIcon);
-    iconsContainer.appendChild(githubLink);
-  
-    const linkedinLink = document.createElement('a');
-    linkedinLink.href = 'https://www.linkedin.com/in/ferpastordev/';
-    linkedinLink.target = '_blank';
-    const linkedinIcon = document.createElement('i');
-    linkedinIcon.classList.add('fa-brands fa-linkedin');
-    linkedinLink.appendChild(linkedinIcon);
-    iconsContainer.appendChild(linkedinLink);
-  });
-  
+// Ocultar el botón inicialmente
+downloadBtn.style.opacity = '0';
+downloadBtn.style.transform = 'translateY(80px)';
+
+// Mostrar el botón después de que el contenido se haya cargado
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    downloadBtn.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+    downloadBtn.style.opacity = '1';
+    downloadBtn.style.transform = 'translateY(0)';
+  }, 1000); // Ajusta el tiempo de retraso según tus necesidades
+});
+
+// Abrir la URL del archivo al hacer clic en el botón
+downloadBtn.addEventListener('click', function() {
+  window.open('https://ruta-al-archivo.pdf');
+});
+
+
