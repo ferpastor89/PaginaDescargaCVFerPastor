f66e818a5dcdb84ca0e922573ef1c5959ec34fa1
Fernando Pastor <fernandoandres.pastor@gmail.com>
2023-05-31 02:35:44 -0300

    Cambios varios

--- public/index.js
+++ public/index.js
@@ -1,64 +1,36 @@
-const switchInput = document.getElementById('switch');
-const contentContainer = document.querySelector('.container');
-let isEnglish = false;
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleSwitch = document.getElementById('switch');
+  const contentContainer = document.getElementById('contentContainer');
+  const downloadBtn = document.getElementById('downloadBtn');
+  const iconContainer = document.querySelector('.icon-container');
+  let currentLanguage = 'es';
 
-switchInput.addEventListener('change', () => {
-  isEnglish = switchInput.checked;
-  loadContent();
+  toggleSwitch.addEventListener('change', () => {
+    currentLanguage = toggleSwitch.checked ? 'en' : 'es';
+    renderContent();
+  });
+
+  const languageContent = {
+    en: {
+      title: "Hi, I'm Fernando Pastor!",
+      subtitle: "Frontend Developer",
+      description: "Here below, I invite you to download my resume.",
+      downloadText: "Download CV"
+    },
+    es: {
+      title: "¡Hola, soy Fernando Pastor!",
+      subtitle: "Desarrollador Frontend",
+      description: "Acá abajo te invito a que descargues mi CV",
+      downloadText: "Descargar CV"
+    }
+  };
+
+  const renderContent = () => {
+    const { title, subtitle, description, downloadText } = languageContent[currentLanguage];
+    contentContainer.innerHTML = `<h1>${title}</h1><h2>${subtitle}</h2><h3>${description}</h3>`;
+    downloadBtn.innerHTML = downloadText;
+    iconContainer.style.display = 'flex';
+  };
+
+  renderContent();
 });
-
-function loadContent() {
-  const url = isEnglish ? 'english.html' : 'index.html';
-  fetchContent(url)
-    .then(responseText => {
-      contentContainer.innerHTML = responseText;
-    })
-    .catch(error => {
-      console.error('Error al cargar el archivo:', error);
-    });
-}
-
-function fetchContent(url) {
-  return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.text();
-      }
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    });
-}
-
-// Cargar contenido inicial
-window.addEventListener('DOMContentLoaded', () => {
-  loadContent();
-});
-
-
-
-
-
-
-
-
-const downloadBtn = document.getElementById('downloadBtn');
-
-document.getElementById('downloadBtn').addEventListener('click', function() {
-  window.open('https://firebasestorage.googleapis.com/v0/b/ferdownloadcv.appspot.com/o/CV%20Fernando%20Pastor.pdf?alt=media&token=52f800ec-84b0-4ccc-8432-708cfc15dfae', '_blank');
-});
-
-// Ocultar el botón inicialmente
-downloadBtn.style.opacity = '0';
-downloadBtn.style.transform = 'translateY(80px)';
-
-// Mostrar el botón después de que el contenido se haya cargado
-document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(function() {
-    downloadBtn.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
-    downloadBtn.style.opacity = '1';
-    downloadBtn.style.transform = 'translateY(0)';
-  }, 1000); // Ajusta el tiempo de retraso según tus necesidades
-});
-
-
-
-
